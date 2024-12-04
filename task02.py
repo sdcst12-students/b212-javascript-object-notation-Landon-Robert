b0c67e3d38ceaf9d37fdf01eef2f32e286a0ea69
Landon-Robert <159073826+Landon-Robert@users.noreply.github.com>
2024-12-04 11:45:17 -0800

    almost done 2

--- task02.py
+++ task02.py
@@ -7,9 +7,14 @@ import json
 # You can google the difference between POST and GET requests
 
 req = requests.get('http://sdcaf.hungrybeagle.com/menu.php')
-json.loads(req)
 data = req.text
-
+importedData = json.loads(data)
+print(type(importedData))
+print("Menu:")
+for i in importedData['menu']:
+    for j in importedData['menu'][importedData['menu'].index(i)]:
+        print(importedData['menu'][importedData['menu'].index(i)][j])
+    print('='*20)
 # Use the json encoded data that is retrieved from this website and print out the weekly menu
 # You will need to decipher the json decoded data to determine what information the 
 # dictionary object contains
